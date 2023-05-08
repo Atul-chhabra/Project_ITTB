a4eeef62514399ea29b4b2acc4373ead683fde86
Atul chhabra <atulchhabra26@gmail.com>
2023-05-08 11:46:58 +0530

    Add project

---
+++ src/registration/registration.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css'],
+})
+export class RegistrationComponent implements OnInit {
+  fullName: string = '';
+  email: string = '';
+  phone: string = '';
+  course: string = '';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    const obj = {
+      Name: this.fullName,
+      Email: this.email,
+      Phone: this.phone,
+      Course: this.course,
+    };
+    console.log(obj);
+    this.http
+      .post(
+        'https://iitbapim.azure-api.net/manual/paths/invoke',
+        JSON.stringify(obj),
+        {
+          headers: {
+            'Ocp-Apim-Subscription-Key': 'ff5cb4f77263439ca10b06d564852164',
+          },
+        }
+      )
+      .subscribe((response) => {
+        console.log(response);
+        alert('Registration Successful');
+      });
+  }
+}

5db626fb27336a206ddcd68f5a713a015dd9a4a9
“realsanket” <joshisanket097@gmail.com>
2023-05-16 22:30:15 +0300

    updated app component and angular.json

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/header/header.component';
+import { FooterComponent } from 'src/footer/footer.component';
+import { PageBannerComponent } from 'src/page-banner/page-banner.component';
+import { RegistrationComponent } from 'src/registration/registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageBannerComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
